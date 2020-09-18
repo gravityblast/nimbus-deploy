bfbca46524f7e73d4fa561a4030107f588b2b138
Andrea Franz <andrea@gravityblast.com>
2020-09-18 13:22:10 +0200

    switch to master branch and build ncli_db

--- docker/nimbus/build-nimbus.sh
+++ docker/nimbus/build-nimbus.sh
@@ -3,8 +3,10 @@ export NIMFLAGS="-d:insecure";
 echo "fetching..." && \
   git fetch && \
   echo "resetting..." && \
-  git reset --hard origin/devel && \
+  git reset --hard origin/master && \
   echo "updating..." && \
   make update && \
-  echo "building... (NIMFLAGS: $NIMFLAGS)" && \
-  make beacon_node
+  echo "building beacon_node... (NIMFLAGS: $NIMFLAGS)" && \
+  make beacon_node && \
+  echo "building ncli_db..." && \
+  make ncli_db
